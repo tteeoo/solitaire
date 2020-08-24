b5b08f23f3bbfafa8812d4e236fc46ad394d24e9
Theo Henson <theodorehenson@protonmail.com>
2020-08-24 16:25:44 -0400

    Replace f strings with .format, change error messages

--- solitaire.py
+++ solitaire.py
@@ -1,12 +1,12 @@
+import readline
 import random
 import time
 import os
-import shutil
 import time
 
 w = 0
 
-#Card init
+# Card init
 suits = ("s","h","c","d")
 nums = ("a","2","3","4","5","6","7","8","9","t","j","q","k")
 cards = []
@@ -14,46 +14,42 @@ for s in suits:
     for n in nums:
         cards.append(s + n)
 
-#(a)Col init
+# Initialize columns and foundations
 cols = []
-acols = []
-for i in ("col1","col2","col3","col4","col5","col6","col7","acols","acolh","acolc","acold"):
-    if i[0] == "a":
-        i = []
-        acols.append(i)
-    else:
-        i = []
-        cols.append(i)
+fcols = []
+for i in range(7):
+    cols.append([])
+for i in range(4):
+    fcols.append([])
 
-#Populate
+
+# Populate
 for c in range(len(cols)):
     for i in range(c + 1):
-        if i == c:
-            card = random.choice(cards)
-            cols[c].append(card + "u")
-        else:
-            card = random.choice(cards)
-            cols[c].append(card + "d")
+        card = random.choice(cards)
+        if i == c: cols[c].append(card + "u")
+        else: cols[c].append(card + "d")
         cards.remove(card)
 
-#Shuffle
+# Shuffle
 random.shuffle(cards)
 
-#Render
+# Render
 def render():
-    os.system("clear")
-    global cols, cards, acols, w
-    #Check win
-    try:
-        for a in range(len(acols)):
-            if acols[a][-1][1] == "k":
-                w += 1
-    except IndexError:
-        pass
-    if w >= 4:
-        win()
+    global cols, cards, fcols, w
 
-    #Flip over
+    # Clear terminal
+    os.system("clear")
+
+    # Check win
+    try:
+        for i in range(len(fcols)):
+            if fcols[i][-1][1] == "k":
+                w += 1
+    except IndexError: pass
+    if w >= 4: win()
+
+    # Flip over end cards
     for c in range(len(cols)):
         if cols[c] != []:
             if cols[c][-1][2] == "d":
@@ -63,7 +59,7 @@ def render():
                 "".join(card)
                 cols[c].append(card)
 
-    #Output cols init
+    # Initialize output columns
     olen = []
     for i in range(len(cols)):
         olen.append(len(cols[i]))
@@ -79,9 +75,9 @@ def render():
         for i in range(len(cols[c])):
             if cols[c][i][2] == "u":
                 if color(cols[c][i]) == "red":
-                    orows[i] += f"[\033[41m\033[30m{cols[c][i][0]}{cols[c][i][1]}\033[0m]"
+                    orows[i] += "[\033[41m\033[30m{}{}\033[0m]".format(cols[c][i][0], cols[c][i][1])
                 if color(cols[c][i]) == "black":
-                    orows[i] += f"[\033[40m\033[39m{cols[c][i][0]}{cols[c][i][1]}\033[0m]"
+                    orows[i] += "[\033[40m\033[39m{}{}\033[0m]".format(cols[c][i][0], cols[c][i][1])
             elif cols[c][i][2] == "d":
                 orows[i] += "[--]"
             else:
@@ -93,28 +89,28 @@ def render():
             except ValueError:
                 pass
 
-    #Output acols init
-    oacols = ""
-    for c in range(len(acols)):
+    # Initialize output foundation columns
+    ofcols = ""
+    for c in range(len(fcols)):
         try:
-            if color(acols[c][-1]) == "red":
-                oacols += f"[\033[41m\033[30m{acols[c][-1][0]}{acols[c][-1][1]}\033[0m]"
-            if color(acols[c][-1]) == "black":
-                oacols += f"[\033[40m\033[39m{acols[c][-1][0]}{acols[c][-1][1]}\033[0m]"
+            if color(fcols[c][-1]) == "red":
+                ofcols += "[\033[41m\033[30m{}{}\033[0m]".format(fcols[c][-1][0], fcols[c][-1][1])
+            if color(fcols[c][-1]) == "black":
+                ofcols += "[\033[40m\033[39m{}{}\033[0m]".format(fcols[c][-1][0], fcols[c][-1][1])
         except IndexError:
-            oacols += "[  ]"
+            ofcols += "[  ]"
     
-    #Output cards init
+    # Initialize output deck
     if cards == []:
         ocards = "[  ]"
     elif color(cards[0]) == "red":
-        ocards = f"[\033[41m\033[30m{cards[0][0]}{cards[0][1]}\033[0m]"
+        ocards = "[\033[41m\033[30m{}{}\033[0m]".format(cards[0][0], cards[0][1])
     elif color(cards[0]) == "black":
-        ocards = f"[\033[40m\033[39m{cards[0][0]}{cards[0][1]}\033[0m]"
+        ocards = "[\033[40m\033[39m{}{}\033[0m]".format(cards[0][0], cards[0][1])
     else:
         pass
 
-    #Print
+    # Print board
     print("-01--02--03--04--05--06--07-") 
     for i in range(len(orows)):
         print(orows[i].rjust(28), end="")
@@ -122,10 +118,11 @@ def render():
             print(" -0"+str(i+1)+"-")
         else:
             print(" -"+str(i+1)+"-")
-    print(f"\n{oacols}")
-    print(f"{ocards}\n")
+    print(ofcols)
+    print(ocards)
+    print("")
 
-#Prompt
+# Prompt
 def prompt():
     global cols, w
     command = input("solitaire> ")
@@ -140,7 +137,7 @@ def prompt():
             print("Invalid move arguments")
     prompt()
 
-#Move
+# Move
 def m(command):
     global cols, acols, nums, suits
     if command[0] != "0" and command[0] != "a":
@@ -203,20 +200,20 @@ def m(command):
                 if card[0] == suits[i]:
                     suit = i
             if card[1] != "a":
-                if acols[suit] == []:
-                    print("Only aces can be stacked on empty ace piles")
-                elif nums[nums.index(card[1]) - 1] != nums[nums.index(acols[suit][-1][1])]:
-                    print("Stacked cards in the ace pile must be in increasing order")
+                if fcols[suit] == []:
+                    print("Only aces can be stacked on empty foundations")
+                elif nums[nums.index(card[1]) - 1] != nums[nums.index(fcols[suit][-1][1])]:
+                    print("Cards stacked on the foundation must be in increasing order")
                 else:
                     cols[scol].remove(card)
-                    acols[suit].append(card)
+                    fcols[suit].append(card)
                     render()
             else:
-                if acols[suit] != []:
-                    print("Aces can only be stacked on an empty ace pile")
+                if fcols[suit] != []:
+                    print("Aces can only be stacked on an empty foundation")
                 else:
                     cols[scol].remove(card)
-                    acols[suit].append(card)
+                    fcols[suit].append(card)
                     render()
         else:
             card = cards[0]
@@ -224,25 +221,25 @@ def m(command):
                 if card[0] == suits[i]:
                     suit = i
             if card[1] != "a":
-                if acols[suit] == []:
-                    print("Only aces can be stacked on empty ace piles")
-                elif nums[nums.index(card[1]) - 1] != nums[nums.index(acols[suit][-1][1])]:
-                    print("Stacked cards in the ace pile must be in increasing order")
+                if fcols[suit] == []:
+                    print("Only aces can be stacked on empty foundations")
+                elif nums[nums.index(card[1]) - 1] != nums[nums.index(fcols[suit][-1][1])]:
+                    print("Cards stacked on the foundation must be in increasing order")
                 else:
                     cards.remove(card)
-                    acols[suit].append(card)
+                    fcols[suit].append(card)
                     render()
             else:
-                if acols[suit] != []:
-                    print("Aces can only be stacked on an empty ace pile")
+                if fcols[suit] != []:
+                    print("Aces can only be stacked on an empty foundation")
                 else:
                     cards.remove(card)
-                    acols[suit].append(card)
+                    fcols[suit].append(card)
                     render()
     else:
         print("Invalid parameters")
 
-#Draw
+# Draw
 def d():
     global cards
     try:
@@ -253,25 +250,16 @@ def d():
         pass
     render()
 
-#Color
+# Color
 def color(card):
     if card[0] == "s" or card[0] == "c":
         return "black"
     if card[0] == "h" or card[0] == "d":
         return "red"
 
-#Win
+# Win
 def win():
-    c = 0
-    while 1:
-        size = shutil.get_terminal_size((80, 20))
-        if c % 2 == 0:
-            c += 1
-            print("\033[40m\033[31m You Win \033[0m" * size[0])
-        else:
-            c += 1
-            print("\033[41m\033[30m You Win \033[0m" * size[0])
-        time.sleep(0.5)
+    print("You win!")
 
 render()
 prompt()
